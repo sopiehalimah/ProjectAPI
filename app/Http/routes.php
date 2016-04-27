534f32f0b1adadfc5d721ef29a43c74223618941
sopiehalimah <sopiehalimah@gmail.com>
2016-04-27 22:17:03 +0700

    API

---
+++ app/Http/routes.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+
+
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', 'WelcomeController@index');
+
+Route::get('/home', 'HomeController@index');
+
+Route::controllers([
+	'auth' => 'Auth\AuthController',
+	'password' => 'Auth\PasswordController',
+	]);
+
+Route::get('master/currency', 'HomeController@view_Currency');
+
+Route::get('cronGetCurrency', 'HomeController@get_Currency');
+
+Route::get('master/lang', 'HomeController@view_Lang');
+
+Route::get('cronGetLang', 'HomeController@get_Lang');
+
+Route::get('master/country', 'HomeController@view_Country');
+
+Route::get('cronGetCountry', 'HomeController@get_Country');
+
+
+
+Route::auth();
+
+Route::get('/home', 'HomeController@index');
