0047fde768938a411bbfbff2a2ca0e8ccb3b6c8d
sopiehalimah <sopiehalimah@gmail.com>
2016-11-06 14:25:10 +0700

    fixed2

--- resources/views/reservasi/flight.blade.php
+++ resources/views/reservasi/flight.blade.php
@@ -77,68 +77,80 @@
 </div>
 @endsection
 @section('footer')
- <script type="text/javascript">
-        function check_type() {
-          var tipe = $('#type').val();
-          if (tipe=='OW') {
-            $('#arrive_date').removeClass('datepicker');
-            $('#arrive_date').prop('disabled',true);
-          }
-          else {
-            $('#arrive_date').addClass('datepicker');
-            $('#arrive_date').prop('disabled',false);
-          }
-        }
-        $(function(){
-          $('select').material_select();
-          $('.datepicker').pickadate({
-            selectMonths: true,
-            selectYears: 15
-          });
-          $('#search_flight').submit(function(e){
-            e.preventDefault();
-            $.ajax({
-              url: '{{ route('ajax_search_flight') }}',
-              type: 'post',
-              data: $(this).serializeArray(),
-              dataType: 'json',
-              success:function(data){
-                var hasil_depart = data.departures;
-                var res_depart = hasil_depart.result;
-                var html = '<ul class="collapsible popout" data-collapsible="accordion">';
-                for (data in res_depart) {
-                  html += '<li>';
-                  html += '<div class="collapsible-header">';
-                  html += '<img src="'+res_depart[data].image+'">';
-                  html += res_depart[data].airlines_name+' ('+res_depart[data].full_via+') with '+res_depart[data].flight_number;
-                  html += '<div class="right">'+res_depart[data].markup_price_string+'</div>';
-                  html += '</div>';
-                  html += '<div class="collapsible-body" style="padding:10px">';
-                  var flights = res_depart[data].flight_infos;
-                  var flight_infos = flights.flight_info;
-                  for (info in flight_infos) {
-                    html += '<h5>'+flight_infos[info].flight_number+'</h5>';
-                    html += '<div class="right">';
-                    html += flight_infos[info].arrival_city+' at '+flight_infos[info].simple_arrival_time;
-                    html += '</div>';
-                    html += '<div class="left">';
-                    html += flight_infos[info].departure_city+' at '+flight_infos[info].simple_departure_time;
-                    html += '</div>';
-                    html += '<br>';
-                    html += '<hr>';
-                  }
-                  html += '</div>';
-                  html += '</li>';
-                }
-                html += '</ul>';
-                $('#result').html(html);
-                $('.collapsible').collapsible();
-              },
-              error:function(){
-                alert('Ajax Error');
-              }
-            });
-          });
-        });
-      </script>
+<script type="text/javascript">
+	$(".for_date").datepicker();
+
+	function check_type(){
+		var tipe = $("#type").val();
+		if(tipe=='OW'){
+			$("#return_date").prop('disabled',true);
+		}else{
+			$("#return_date").prop('disabled',false);
+		}
+	}
+	check_type();
+
+	function search(){
+		$.ajax({
+			url:'{{route("ajax_search_flight")}}',
+			type:'POST',
+			data:{
+					from:$("#form").val(),
+					to:$("#to").val(),
+					type:$("#type").val(),
+					depart_date:$("#depart_date").val(),
+					return_date:$("#return_date").val(),
+					adult:$("#adult").val(),
+					child:$("#child").val(),
+					infant:$("#infant").val(),
+					_token:'{{csrf_token()}}'
+				}, 
+				dataType:'json',
+
+				success:function(data){
+
+					var hasil_depart = data.departures;
+
+					var res_depart = hasil_depart.result;
+
+                        var html = '<ul class="collapsible popout" data-collapsible="accordion">';
+                        //looping hasil departures
+                        for(data in res_depart){
+                              //tampilkan dalam bentuk collapsible
+                              html += '<li>';
+                              html += '<div class="collapsible-header">';
+                              html += '<img src="'+res_depart[data].image+'">'
+                              html += res_depart[data].airlines_name+ ' ('+res_depart[data].full_via+') with '+res_depart[data].flight_number;
+                              html += '<div class="right">'+res_depart[data].markup_price_string+'</div>';
+                              html += '</div>';
+                              html += '<div class="collapsible-body" style="padding:10px;">';
+                              //ambil array object flight info
+                              var flights = res_depart[data].flight_infos;
+                              var flight_infos = flights.flight_info;
+                              //looping isi array flight info
+                              for(info in flight_infos){
+                                    //masukkan ke dalam body collapsible
+                                    html += '<h5>'+flight_infos[info].flight_number+'</h5>';
+                                    html += '<div class="right">';
+                                    html += flight_infos[info].arrival_city+' at '
+                                          +flight_infos[info].simple_arrival_time;
+                                    html += '</div>';
+                                    html += '<div class="left">';
+                                    html += flight_infos[info].departure_city+' at '
+                                          +flight_infos[info].simple_departure_time;
+                                    html += '</div>';
+                                    html += '<br>';
+                                    html += '<hr>';
+                              }
+                              html += '</div>';
+                              html += '</li>';
+                        }
+                        html += '</ul>';
+					$("#result").html(html);
+					$('.collapsible').collapsible();
+				}
+		})
+	}
+
+</script>
 @endsection
